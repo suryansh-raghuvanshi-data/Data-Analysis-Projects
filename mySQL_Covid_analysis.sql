d094b6c2533226a2036aacb3b91fe463e7e53438
suryansh-raghuvanshi-data <113336985+suryansh-raghuvanshi-data@users.noreply.github.com>
2023-06-07 12:30:07 +0530

    mySQL_Covid_analysis.sql

--- mySQL_Covid_analysis.sql
+++ mySQL_Covid_analysis.sql
@@ -117,31 +117,32 @@ From PopvsVac
 
 
 -- Using Temp Table to perform Calculation on Partition By in previous query
+--Alternate: Create View or  Create Temporary Table
 
-DROP Table if exists #PercentPopulationVaccinated
-Create Table #PercentPopulationVaccinated
-(
-Continent nvarchar(255),
-Location nvarchar(255),
-Date datetime,
-Population numeric,
-New_vaccinations numeric,
-RollingPeopleVaccinated numeric
-)
+-- DROP Table if exists #PercentPopulationVaccinated
+-- Create Table #PercentPopulationVaccinated
+-- (
+-- Continent nvarchar(255),
+-- Location nvarchar(255),
+-- Date datetime,
+-- Population numeric,
+-- New_vaccinations numeric,
+-- RollingPeopleVaccinated numeric
+-- )
 
-Insert into #PercentPopulationVaccinated
-Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
-, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
---, (RollingPeopleVaccinated/population)*100
-From CovidDeaths dea
-Join CovidVaccinations vac
-	On dea.location = vac.location
-	and dea.date = vac.date
---where dea.continent is not null 
---order by 2,3
+-- Insert into #PercentPopulationVaccinated
+-- Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+-- , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+-- --, (RollingPeopleVaccinated/population)*100
+-- From CovidDeaths dea
+-- Join CovidVaccinations vac
+-- 	On dea.location = vac.location
+-- 	and dea.date = vac.date
+-- --where dea.continent is not null 
+-- --order by 2,3
 
-Select *, (RollingPeopleVaccinated/Population)*100
-From #PercentPopulationVaccinated
+-- Select *, (RollingPeopleVaccinated/Population)*100
+-- From #PercentPopulationVaccinated
 
 
 
